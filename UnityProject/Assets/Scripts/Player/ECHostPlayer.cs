753be19c89c2c1cd666430207c458de0adcdc3a7
yangfeiyu <fenghenyuleidie@126.com>
2016-02-26 18:32:30 +0800

    添加行动

---
+++ UnityProject/Assets/Scripts/Player/ECHostPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ECHostPlayer :  ECObject{
+	private ECHostPlayer(){}
+	ECHostPlayer m_inst;
+	FSMList m_FSMList = new FSMList();
+	public static ECHostPlayer Instance ()
+	{
+	   if (m_inst==null) 
+	   {
+	       m_inst=new ECHostPlayer();
+	   }
+	   return m_inst;
+	}
+}
