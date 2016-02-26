7592448c2ec30da6b8c431b09bab70a2a9410e08
yangfeiyu <fenghenyuleidie@126.com>
2016-02-26 18:36:25 +0800

    version0.2  添加行为

--- UnityProject/Assets/Scripts/ECObject.cs
+++ UnityProject/Assets/Scripts/ECObject.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour {
+public class ECObject : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ UnityProject/Assets/Scripts/FSM/FSMMove.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FSMMove : FSMUnit {
+	Vector3 m_desPos;
+	ECObject  m_objct;
+	int speed = 2;
+	public ECObject Mover
+	{
+	  get{return m_object;}
+	  set{m_object = value;}
+	}
+
+	public Vector3 DesPos
+	{
+	   set
+	   {
+              DesPos=value;
+	   }
+	}
+	
+	void Init()
+	{
+	   Priority = FSMPRIORITYTYPE.ACTION_MOVE;
+           FSMType = "move";
+	}
+	public FSMMove(){Init();}
+	public FSMMove(ECObject mover, Vector3 desPos)
+	{
+	   Init();
+	   this.DesPos=desPos;
+	   this.Mover = mover;
+	}
+	
+	bool IsNearDesPos()
+	{
+		
+	   if(System.Math.Abs(m_objct.CurPos.x-m_desPos.x)<2)
+	   {
+		   return true;
+	   }
+	   return false;
+
+	}
+	public override bool Run()
+	{
+           if(IsNearDesPos())
+	   {
+		return false;
+	   }
+	   m_object
+	   m_objct.AddAction(
+	}
+}
