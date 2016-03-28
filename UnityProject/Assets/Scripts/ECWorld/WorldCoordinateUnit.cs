df716cfb903bb3e66202a230dba61a841077aa2f
yangfeiyu <fenghenyuleidie@126.com>
2016-03-29 00:01:52 +0800

    ssss

--- UnityProject/Assets/Scripts/ECWorld/WorldCoordinateUnit.cs
+++ UnityProject/Assets/Scripts/ECWorld/WorldCoordinateUnit.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WorldCoordinateUnit  {
 
 public:
 	 int Legnth = 5;
+      int DartNum = 0;
+	  ArrayList list = new ArrayList(10);
 
+	  void AddSkill(Skill s)
+	  {
+	  	list[DartNum]=s;
+	  	DartNum=DartNum+1;
+	  }
+	  
+
+	  void Clear()
+	  {
+
+	  }
 }
